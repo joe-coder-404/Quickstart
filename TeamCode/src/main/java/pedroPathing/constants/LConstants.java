9327afbad734af26862a8e94a17302bfe34233a6
BaronClaps <126834072+baronclaps@users.noreply.github.com>
2025-04-23 20:14:35 -0500

    Update LocalizationTest.java

--- TeamCode/src/main/java/pedroPathing/constants/LConstants.java
+++ TeamCode/src/main/java/pedroPathing/constants/LConstants.java
@@ -2,9 +2,22 @@ package pedroPathing.constants;
 
 import com.pedropathing.localization.*;
 import com.pedropathing.localization.constants.*;
+import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class LConstants {
     static {
+
+        OTOSConstants.useCorrectedOTOSClass = false;
+        OTOSConstants.hardwareMapName = "sensor_otos";
+        OTOSConstants.linearUnit = DistanceUnit.MM;
+        OTOSConstants.angleUnit = AngleUnit.DEGREES;
+        OTOSConstants.offset = new SparkFunOTOS.Pose2D(193, 46, Math.PI / 2);
+        OTOSConstants.linearScalar = 1.0;
+        OTOSConstants.angularScalar = 1.0;
+
         ThreeWheelConstants.forwardTicksToInches = .001989436789;
         ThreeWheelConstants.strafeTicksToInches = .001989436789;
         ThreeWheelConstants.turnTicksToInches = .001989436789;
